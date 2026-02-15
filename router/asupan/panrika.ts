18c1cfc2dd9e4a983b6ebe67dad133b54d2098b7
KazzTzyPrivat7777 <badegu3451@gmail.com>
2026-02-16 06:26:38 +0700

    Update file router/asupan/panrika.ts

---
+++ router/asupan/panrika.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+
+export default async function panrikaHandler(req: Request, res: Response) {
+  try {
+    const jsonPath = path.join(__dirname, "panrika.json");
+    const data = fs.readFileSync(jsonPath, "utf-8");
+    const links: { url: string }[] = JSON.parse(data);
+
+    if (!links.length) {
+      return res.status(404).json({
+        status: false,
+        message: "Tidak ada video"
+      });
+    }
+
+    const randomItem = links[Math.floor(Math.random() * links.length)];
+    res.json({
+      status: true,
+      url: randomItem.url
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      status: false,
+      message: err.message
+    });
+  }
+}
+	
