533306fe8364a4682d8154b3041b64e8d7f1805b
KazzTzyPrivat7777 <badegu3451@gmail.com>
2026-02-16 07:29:19 +0700

    Update file KazzHtps-main/router/anime/bully.ts

---
+++ KazzHtps-main/router/anime/bully.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import fetch from 'node-fetch'
+
+export default async function handler(req: Request, res: Response) {
+  try {
+    const r = await fetch('https://waifu.pics/api/sfw/bully')
+    const j = await r.json()
+    const img = await fetch(j.url)
+    const buf = Buffer.from(await img.arrayBuffer())
+    res.set('Content-Type', 'image/png')
+    res.send(buf)
+  } catch {
+    res.status(500).json({ status: false })
+  }
+}
